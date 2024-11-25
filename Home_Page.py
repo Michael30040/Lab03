e06226c8ad65828a86c23fbfc800f08b4c15b406
Michael30040 <michael.charles5040@gmail.com>
2024-11-25 13:58:02 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team 36, Web Development - Section D")
+st.subheader("Michael Charles, Abhiram Kapaganty")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **Portfolio - Michael Charles**: A portfolio showcasing projects, skills, and experience of Michael Charles.
+2. **Portfolio - Abhiram Kapaganty**: A portfolio showcasing projects, skills, and experience of Abhiram Kapaganty.
+3. **API Data Analysis**: A page analyzing data from a web API, with user inputs and dynamic visualizations.
+4. **LLM Integration**: A page using Google Gemini LLM for processing and chatbot interactions based on API data.
+
+""")
+
